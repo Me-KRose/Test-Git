41552542079c74db47f64749355736643a764373
X541-NB <mm7082418@gmail.com>
2026-06-24 23:32:41 -0800

    My first COMMIT

---
+++ main.c
@@ -0,0 +1,10 @@
+#include "main.h"
+#include <SDL3/SDL.h>
+
+int main(int argc, char *argv[])
+{
+    printf("Hello World!");
+    int num = 63;
+    printf("\nThe number is = %d",num);
+    return 0;
+}
