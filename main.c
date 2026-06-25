9c05c931b8c4391e1dae5562a9dfaa1c21a9548c
X541-NB <mm7082418@gmail.com>
2026-06-25 02:28:03 -0800

    the second Commit

--- main.c
+++ main.c
@@ -1,8 +1,12 @@
 #include "main.h"
-#include <SDL3/SDL.h>
+// #include <SDL2/SDL.h>
 
 int main(int argc, char *argv[])
 {
+    // if(SDL_Init(SDL_INIT_EVERYTHING) < 0)
+    // {
+    
+    // }
     printf("Hello World!");
     int num = 63;
     printf("\nThe number is = %d",num);
